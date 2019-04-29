d54d156d96c821459f14e7dfa872d12f08f0518b
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-04-29 19:58:26 -0400

    see if list is a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks a linked list to see if its a cycle
+ * @list: head of linked list
+ *
+ * Return: 1 (suscess) 0 (failure)
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *first, *bot;
+
+	bot = first = list;
+	while (bot != NULL)
+	{
+		if (bot->next == NULL)
+			return (0);
+		bot = bot->next;
+		if (bot == first)
+			return (1);
+	}
+	return (0);
+}
