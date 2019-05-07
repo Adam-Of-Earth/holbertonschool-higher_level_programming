b9dfc2bcb1b85ffa31a75e6efbc0a615b6b0efba
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-05-07 16:31:48 -0400

    makes change in new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    ret = my_list[:]
     if idx < 0 or idx >= len(my_list):
-        return(my_list)
+        return(ret)
     else:
-        copy = my_list
-        copy[idx] = element
-        return(copy)
+        ret[idx] = element
+        return(ret)
