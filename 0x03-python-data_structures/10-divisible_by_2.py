5820aeada8548aec63cb32dbd6415bed23aa54ea
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-05-07 21:22:13 -0400

    checks if true

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     mylist = []
-    for i in mylist:
+    for i in my_list:
         if i % 2 == 0:
             mylist.append(True)
         else:
