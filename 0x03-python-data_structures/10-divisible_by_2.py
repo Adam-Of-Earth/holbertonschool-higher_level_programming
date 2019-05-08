522ffae8c1152fa3fe94f24dddf6bee897a36c14
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-05-07 21:07:47 -0400

    checks if list is even

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    mylist = []
+    for i in mylist:
+        if i % 2 == 0:
+            mylist.append(True)
+        else:
+            mylist.append(False)
+    return mylist
