50ffebb0a966b70ad32ce31885918ad96e975533
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-05-21 14:00:33 -0400

    format and 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a class Square
+
+Description of a square class
+
+"""
+
+class Square:
+    """square shape
+
+    infomation about the square and how to use it
+
+    """
+    pass
