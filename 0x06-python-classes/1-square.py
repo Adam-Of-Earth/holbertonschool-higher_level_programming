ed366be4b9e5024ffc303ac5d236b3aa2014c782
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-05-21 14:33:13 -0400

    add class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a class Square
+
+Description of a square class
+
+"""
+
+class Square:
+    """square shape
+
+    infomation about the square and how to use it
+
+    """
+    def __init__(self, size):
+        """Create a square size large
+
+        size(int): how large the size wil be
+
+        """
+        self.__size = size
