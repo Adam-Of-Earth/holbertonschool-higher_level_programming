082b6075990ad6f4f5a4dcb6b608ac61e369cd0e
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-05-21 15:14:35 -0400

    adds errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a class Square
+
+Description of a square class
+
+"""
+
+class Square:
+    """square shape
+
+    infomation about the square and how to use it
+
+    """
+
+    def __init__(self, size):
+        """Create a square size large
+
+        size(int): how large the size wil be
+
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
