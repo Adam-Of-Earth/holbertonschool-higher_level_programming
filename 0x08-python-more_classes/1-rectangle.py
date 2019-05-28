eb2da8d3a24e3081402a9fd97508bafc3cbead77
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-05-28 16:51:24 -0400

    defines heigt and width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""class defineing a rectangle"""
+
+
+class Rectangle:
+    """rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """initialize rectangle
+
+        Args:
+        height(int): rectangle height
+        width(int): rectangel width
+        """
+
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """gets the height"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, val):
+        """sets height"""
+        if not isinstance(val, int):
+            raise TypeError("height must be an integer")
+        if val < 0:
+            raise TypeError("height must be >= 0")
+        self.__height = val
+
+    @property
+    def width(self):
+        """gets the width"""
+        return self.__width
+
+    @width.setter
+    def width(self,val):
+        """sets width"""
+        if not isinstance(val, int):
+            raise TypeError("width must be an integer")
+        if val < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = val
