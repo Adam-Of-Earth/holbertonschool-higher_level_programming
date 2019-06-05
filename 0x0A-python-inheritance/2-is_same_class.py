0fad88cbcce5386d21ee9ce9498677d5a7d5056d
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-06-05 16:12:32 -0400

    where inherited form

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """checks to see if object is an instanve"""
 
+
 def is_same_class(obj, a_class):
     """fuction to check object"""
     return type(obj) is a_class
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""hecks inheritence of an object"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks inheritence of object"""
+    return isinstance(obj, a_class)
