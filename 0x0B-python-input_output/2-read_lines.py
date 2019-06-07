97a897313053a61ffd1569c4bc626185e094bdfb
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-06-06 23:17:40 -0400

    reads lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,4 +3,10 @@
 
 
 def read_lines(filename="", nb_lines=0):
-    
+    """prints lines from a file"""
+    with open(filename, encoding="UTF-8") as myFile:
+        if nb_lines <= 0:
+            print(myFile.read(), end="")
+            return
+        for i in range(nb_lines):
+            print(myFile.readline(), end="")
