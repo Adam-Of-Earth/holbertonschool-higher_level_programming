eb2530c71847493c024781affca793dd354458b2
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-06-06 21:01:04 -0400

    struct and 0

---
+++ 0x0B-python-imput_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module to read text file"""
+
+
+def read_file(filename=""):
+    """fuction to read file"""
+
+    with open(filename, encoding="utf-8") as myFile:
+              print(myFile.read(), end="")
