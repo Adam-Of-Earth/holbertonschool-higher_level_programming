c454431ffb6c96617d4605311b365503e5bad945
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-06-12 18:31:56 -0400

    new str method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,4 +87,8 @@ class Rectangle(Base):
         for _ in range(self.__height):
             print(" " * self.x + "#" * self.__width)
 
+    def __str__(self):
+        """return representation of a rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
     
