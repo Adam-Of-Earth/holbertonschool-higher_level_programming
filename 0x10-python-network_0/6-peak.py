ce8b9b22cb7fad3956a2aa4240b1fda187119b60
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-08-20 23:39:39 -0400

    finds a peck in a list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""finds the peak in a list"""
+
+
+def find_peak(list_of_integers):
+    """Driver Function for peak"""
+    length = len(list_of_integers)
+    if length < 1:
+        return ("None")
+    result = get_peak(list_of_integers, 0, length - 1, length)
+    return (result)
+
+
+def get_peak(int_list, low, high, n):
+    """Work function for peak"""
+    mid = int(low + (high - low)/2)
+
+    if ((mid == 0 or int_list[mid - 1] <= int_list[mid]) and (
+            mid == n - 1 or int_list[mid + 1] <= int_list[mid])):
+        return (int_list[mid])
+
+    elif (mid > 0 and int_list[mid - 1] > int_list[mid]):
+        return get_peak(int_list, low, (mid - 1), n)
+
+    else:
+        return get_peak(int_list, (mid + 1), high, n)
