639bb93d053eac5fdab5dc6eeea25212e981ccb0
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-08-26 18:34:27 -0400

    added var

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """sends POST request to url"""
 
-from sys import argv
+import sys
 import urllib.parse
 import urllib.request
 
 
-data = urllib.parse.urlencode(email)
-data = data.encode('utf-8')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    print(response.read().decode('utf-8'))
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = {'email': sys.argv[2]}
+    
+    data = urllib.parse.urlencode(email)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
