73933efe8e2638eee8ec4645d45726a1648fb3e4
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-08-26 20:21:28 -0400

    display body of URL

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,6 @@ import urllib.request
 if __name__ == '__main__':
     url = sys.argv[1]
     email = {'email': sys.argv[2]}
-    
     data = urllib.parse.urlencode(email)
     data = data.encode('utf-8')
     req = urllib.request.Request(url, data)
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""displays body of a URL"""
+
+import urllib.error
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    request = request.Request(url)
+    try:
+        with request.urlopen(reqst) as response:
+            print(response.read().decode('urf-8'))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code))
