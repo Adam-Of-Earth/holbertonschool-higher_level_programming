77428a2b03784f45316ed866674bcc916490beaf
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-08-26 23:27:28 -0400

    spelling fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == '__main__':
     reques = requests.get(sys.argv[1])
     if reques.status_code >= 400:
-        print("Error code: {}".format(reqst.status_code))
+        print("Error code: {}".format(reques.status_code))
     else:
         print(reqst.text)
