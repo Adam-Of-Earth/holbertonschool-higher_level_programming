cd20fd27368e8eb7db7f06f1d82be7f5174b9367
root <root@LAPTOP-CEF3OEJ9.localdomain>
2019-08-26 23:23:06 -0400

    displays body handles error code 400+

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""sends a request to URL and displays the response"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    reques = requests.get(sys.argv[1])
+    if reques.status_code >= 400:
+        print("Error code: {}".format(reqst.status_code))
+    else:
+        print(reqst.text)
