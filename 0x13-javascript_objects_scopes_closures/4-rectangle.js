bfbde9241efc761a1cdaaa55f35806e02ef39998
root <root@LAPTOP-CEF3OEJ9.localdomain>
2020-02-10 21:02:54 -0500

    three and four

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const row = 'X'.repeat(this.width);
+    for (let index = 0; index < this.height; index++) {
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
